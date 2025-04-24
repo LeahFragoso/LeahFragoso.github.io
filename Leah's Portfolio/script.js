7314b6ce558f51ad8bb9ab3e6b8134cf3e590644
Leah Fragoso <leahafragoso@gmail.com>
2025-04-24 19:05:11 -0400

    FINAL

--- Leah's Portfolio/script.js
+++ Leah's Portfolio/script.js
@@ -1,5 +1,5 @@
 const journal = document.querySelector('.journal-container');
-  const trinkets = document.querySelectorAll('.trinket');
+const trinkets = document.querySelectorAll('.trinket');
 
   journal.addEventListener('mouseenter', () => {
     trinkets.forEach(trinket => {
@@ -8,16 +8,27 @@ const journal = document.querySelector('.journal-container');
     });
   });
 
+ 
   journal.addEventListener('mouseleave', () => {
     trinkets.forEach(trinket => {
       trinket.style.opacity = 0;  
       trinket.style.transform = 'translateY(20px) scale(0.7)'; 
     });
   });
-  const menuButton = document.getElementById('menuButton');
+  const menuButton   = document.getElementById('menuButton');
   const dropdownMenu = document.getElementById('dropdownMenu');
-  
-  menuButton.addEventListener('click', () => {
+  const menuContainer = document.getElementById('menuContainer');
+
+
+  menuButton.addEventListener('click', e => {
+    e.stopPropagation();
     dropdownMenu.classList.toggle('hidden');
+  });
+
+
+  document.addEventListener('click', e => {
+    if (!menuContainer.contains(e.target)) {
+      dropdownMenu.classList.add('hidden');
+    }
   });
   
